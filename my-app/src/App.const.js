db0c378c5f1686363b5dc3ee2bf229625b1f4358
Piotr Kristowski <cybszot@wp.pl>
2020-06-17 18:28:34 +0200

    update

---
+++ my-app/src/App.const.js
@@ -0,0 +1,18 @@
+export const zmienna = 'Ala ma kota, milicja ma pałę';
+
+export const zmienna2 = {
+    a: 'pole a',
+    b: 'pole b',
+};
+
+export function testowa(a){
+    alert(a);
+}
+
+const zmienna3 = 'zmienna 3 ';
+const zmienna4 = 'zmienna 4 ';
+
+export {
+    zmienna3,
+    zmienna4,
+}
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {zmienna, zmienna2, zmienna3} from './App.const.js';
+
 
 function App() {
+  //const zmienna = "Ala ma kota";
+ // testowa('witam');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CodeMe! React lessons!
         </p>
+
+        <p>
+          {zmienna}, {zmienna3}, {zmienna2.b}
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
