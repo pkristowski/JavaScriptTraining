f4dc1639e413b238b740a457521a47e2f5d5c712
Piotr Kristowski <cybszot@wp.pl>
2020-06-24 17:38:01 +0200

    Update index.js

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -57,6 +57,37 @@ import React from 'react';
 // }
 
 
+// export default class Click extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             clicks: props.defaultValue,
+//         }
+//     }
+
+
+//     handleClick() {
+//         this.setState({
+//             clicks: this.state.clicks + 1,
+//         })
+//     }
+
+//     render() {
+//         const { clicks } = this.state;
+//         return (
+//             <div>
+//                 <div>
+//                     <strong>{clicks}</strong>
+//                 </div>
+//                 <input onClick={this.handleClick.bind(this)} type="button" value="Kliknij" />
+//             </div>
+//         )
+//     }
+// }
+
+
+
+
 export default class Click extends React.Component {
     constructor(props) {
         super(props);
