3fe8f5cc6477e6a858e853ee0c7a427afe80d871
Piotr Kristowski <cybszot@wp.pl>
2020-07-04 11:43:28 +0200

    Update index.js

--- Hackaton04.07.2020/hackaton0407/src/getData/index.js
+++ Hackaton04.07.2020/hackaton0407/src/getData/index.js
@@ -1,67 +1,25 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { Component } from 'react';
 
-class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      products: []
-    }
-  }
+export default class InputTest extends Component {
+    constructor(props) {
+        super(props);
 
-  componentDidMount() {
-    const apiUrl = 'http://localhost/dev/tcxapp/reactapi/products';
-
-    fetch(apiUrl)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            products: result
-          });
-        },
-        (error) => {
-          this.setState({ error });
+        this.state = {
+            input: ''
         }
-      )
-  }
-
-  render() {
-    const { error, products} = this.state;
-
-    if(error) {
-      return (
-        <div>Error: {error.message}</div>
-      )
-    } else {
-      return(
-        <div>
-          <h2>Product List</h2>
-          <Table>
-            <thead>
-              <tr>
-                <th>#ID</th>
-                <th>Product Name</th>
-                <th>SKU</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map(product => (
-                <tr key={product.id}>
-                  <td>{product.id}</td>
-                  <td>{product.product_name}</td>
-                  <td>{product.sku}</td>
-                  <td>{product.price}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>
-      )
     }
-  }
-}
 
-export default ProductList;
+    handleOnChange = event => {
+        this.setState({
+            input: event.target.value
+        });
+    };
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={this.handleOnChange} />
+                <img src={`https://unsplash.it/150/150?image=${this.state.input}`} />
+            </div>
+        )
+    };
+}
