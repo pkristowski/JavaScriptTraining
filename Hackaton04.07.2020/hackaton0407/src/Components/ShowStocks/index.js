5f2eb6ab4c82f2fbbfea0040b4592281413329e8
Piotr Kristowski <cybszot@wp.pl>
2020-07-18 18:00:09 +0200

    update

--- Hackaton04.07.2020/hackaton0407/src/Components/ShowStocks/index.js
+++ Hackaton04.07.2020/hackaton0407/src/Components/ShowStocks/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './style.css';
+import TradingViewWidget from '../../../node_modules/react-tradingview-widget';
 
-export default function ShowStocks() {
+export default function ShowStock() {
     return (
-        
-    )
-};
+
+        <TradingViewWidget symbol="NASDAQ:AAPL" />
+    );
+}
